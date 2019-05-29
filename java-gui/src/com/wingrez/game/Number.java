cb46b3b10245e99d5d07e4020bb6d4739e8c5985
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-29 17:48:46 +0800

    add c and java project

---
+++ java-gui/src/com/wingrez/game/Number.java
@@ -0,0 +1,33 @@
+package com.wingrez.game;
+
+import java.util.Random;
+public class Number {
+	int numa=0;
+	int numb=0;
+	int numc=0;
+	int numd=0;
+	Random random=new Random();
+	Number(){
+		do {
+			numa=random.nextInt(10);
+			numb=random.nextInt(10);
+			numc=random.nextInt(10);
+			numd=random.nextInt(10);
+		}while(judge(numa,numb,numc,numd)==false);
+	}
+	
+	private boolean judge(int a,int b,int c,int d) {
+		if(a==b || a==c || a==d || b==c || b==d || c==d)
+			return false;
+		else return true;
+	}
+	
+	
+//	public static void main(String[] args) {
+//		new CreateNumber();
+//		System.out.println(numa);
+//		System.out.println(numb);
+//		System.out.println(numc);
+//		System.out.println(numd);
+//	}
+}
---
+++ java-gui/src/com/wingrez/game/StartGame.java
@@ -0,0 +1,8 @@
+package com.wingrez.game;
+
+public class StartGame {
+//	static int status=0;
+	public static void main(String[] args) {
+		new GameWindow();
+	}
+}
